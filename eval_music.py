b9e35ba4ab310e121c1894e0682b7efe5c3340bf
Cralence <mahlerrrr76@gmail.com>
2024-02-14 00:23:37 +0800

    update

---
+++ eval_music.py
@@ -0,0 +1,18 @@
+import torch
+from audioldm_eval import EvaluationHelper
+
+# GPU acceleration is preferred
+device = torch.device(f"cuda:{0}")
+
+generation_result_path = " ../My_Project/exp_result/exp_13_2_e15_musiccap_gs4/music/"
+target_audio_path = "../musiccaps/musicap/"
+
+# Initialize a helper instance
+evaluator = EvaluationHelper(16000, device)
+
+# Perform evaluation, result will be print out and saved as json
+metrics = evaluator.main(
+    generation_result_path,
+    target_audio_path,
+    limit_num=None # If you only intend to evaluate X (int) pairs of data, set limit_num=X
+)
